bf551e1d2e49fbfae6538a19fc592294344d7d6a
davinahouquet <davina.houquet@hotmail.com>
2023-05-04 09:19:35 +0200

    Alignement des offres

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./style.css">
     <title>Landing Page Pricing PDO</title>
 </head>
 <body>
@@ -21,13 +21,14 @@
                     <h3 class="pricing-title">Our Pricing</h3
                     <p class="pricing-text">It is a long established fact that a reader will be of a page when established fact looking at its layout.</p>
                 </div>
-
-                <div class="pricing-packs">
-                    <?php
+            </div>
+            <div class="container-packs">
+                <?php
                     
                     foreach(getAll() as $pricing){
-
-                        echo '<div class="pricing-pack">
+                        
+                        echo '<div class="pricing-packs">
+                         <div class="pricing-pack">
                                 <p class="pack-type"><p>'.$pricing['name'].'</p>
                                     <div class="pack-price">
                                         <sup class="dollar">$</sup>
@@ -43,15 +44,14 @@
                                     <li><div><i class="fa-regular fa-circle-check" id="check-red"></i>Hidden Fees</div><p>'.$pricing['hidden_fees'].'</p></li>
                                 </ul>
                             </div>
-                            <button class="pricing-pack-button"><a href="#">Join Now</a></button>';
+                            <button class="pricing-pack-button"><a href="#">Join Now</a></button>
+                            </div>';
 
                     }
                     
-
                     ?>
+            </div>        
                   
-                </div>
-            </div>
         
 
     </section>
